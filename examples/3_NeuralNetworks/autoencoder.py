671b70e2e942611159f3c024dc473da21d784a23
Norman Heckscher <norman.heckscher@gmail.com>
2017-01-14 23:22:37 +1000

    Refactor autoencoders for TF1.0
    Signed-off-by: Norman Heckscher <norman.heckscher@gmail.com>

--- examples/3_NeuralNetworks/autoencoder.py
+++ examples/3_NeuralNetworks/autoencoder.py
@@ -17,7 +17,7 @@ import matplotlib.pyplot as plt
 
 # Import MNIST data
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
 
 # Parameters
 learning_rate = 0.01
